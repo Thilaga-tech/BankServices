1dcc1718173854608654db477e8e1e18fb52a77f
Ramya ramar <ramya@developer.com>
2021-05-19 19:33:14 +0530

    first commit

---
+++ bankServices/src/main/java/com/bank/bankServices/BankServicesApplication.java
@@ -0,0 +1,15 @@
+package com.bank.bankServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServicesApplication.class, args);
+		
+		System.out.println("Application Started...");
+
+}
+}
